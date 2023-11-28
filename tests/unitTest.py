f86263a9d89ff11763944219c4ced17c560d79b8
abrhamadddis <abrham32@gmail.com>
2023-11-28 22:22:54 +0300

    unit test is added for TestSlackDataLoader

---
+++ tests/unitTest.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+from your_module_name import SlackDataLoader  # Replace 'your_module_name' with the actual name of the module
+
+class TestSlackDataLoader(unittest.TestCase):
+
+    def setUp(self):
+        # Path to the test Slack exported data folder
+        test_data_path = 'path/to/TestSlackExportedData'
+        self.loader = SlackDataLoader(test_data_path)
+
+    def test_get_channels(self):
+        # Test if channels are loaded correctly
+        channels = self.loader.get_channels()
+        self.assertIsNotNone(channels)
+        self.assertIsInstance(channels, list)
+        self.assertGreater(len(channels), 0)
+
+    def test_get_users(self):
+        # Test if users are loaded correctly
+        users = self.loader.get_users()
+        self.assertIsNotNone(users)
+        self.assertIsInstance(users, list)
+        self.assertGreater(len(users), 0)
+
+    # Add more tests as needed for other methods in SlackDataLoader
+
+if __name__ == '__main__':
+    unittest.main()
